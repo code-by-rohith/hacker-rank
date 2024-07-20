abfaa6be1bf235a09f09f792b7f6ff41298f89bc
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-07-20 19:34:36 +0530

    add

---
+++ Mutations.py
@@ -0,0 +1,13 @@
+
+def mutate_string(s,i,c):
+    list1=list(s)
+    list1[i]=c
+    string1="".join(list1)
+
+    return string1
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
