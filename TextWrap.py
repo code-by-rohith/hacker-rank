fa08d80fea015c42941851b1eae176eac31c6410
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-07-21 17:34:19 +0530

    add

--- TextWrap.py
+++ TextWrap.py
@@ -1,7 +1,7 @@
 import textwrap
-
 def wrap(string, max_width):
     return textwrap.fill(string, max_width)
-string = "ABCDEFGHIJKLIMNOQRSTUVWXYZ"
-max_width = 4
-print(wrap(string, max_width))
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
