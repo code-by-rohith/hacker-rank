7698fdbf05f6d4666ce4dc7015ecc98e75ed8826
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-07-26 17:20:04 +0530

    add

--- InfiniteGrid.py
+++ InfiniteGrid.py
@@ -1,4 +1,6 @@
-# Assume there exists infinite grid, you’re given initial position x, y. Inputs will be movements either L or R or U or D. After n inputs, you need to give the current position.
+# Assume there exists infinite grid, you’re given initial position x, y.
+# Inputs will be movements either L or R or U or D. After n inputs,
+# you need to give the current position.
 # •	Input:
 # •	4 5 //initial position x, y
 # •	9 //number of movements
