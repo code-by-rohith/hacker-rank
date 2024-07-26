31855ab58bb71dcbfae9f67411fb2852d16ba498
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-07-26 23:31:56 +0530

    add

--- BikeTrips.py
+++ BikeTrips.py
@@ -1,3 +1,9 @@
+# Given an array of values persons[], each represents the weight of the persons.
+# There will be infinite bikes available.
+# Given a value K which represents the maximum weight that a bike accommodates.
+# Along with that one more condition, a bike can carry two persons at a time.
+# You need to find out the least number of times, the bike trips are made.
+
 def find_trips(arr, weight):
     arr.sort()
     trips = 0
