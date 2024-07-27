c87b420b3463258cde756c426e43f66b47adc0e3
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-07-27 18:02:11 +0530

    add

--- Type_2/Alphabet_Rangoli.py
+++ Type_2/Alphabet_Rangoli.py
@@ -1,16 +1,36 @@
-def rangoli(size):
+# You are given an integer,N . Your task is to print an alphabet rangoli of size N .
+# (Rangoli is a form of Indian folk art based on creation of patterns.)
+# Sample Input
+#
+# 5
+# Sample Output
+#
+# --------e--------
+# ------e-d-e------
+# ----e-d-c-d-e----
+# --e-d-c-b-c-d-e--
+# e-d-c-b-a-b-c-d-e
+# --e-d-c-b-c-d-e--
+# ----e-d-c-d-e----
+# ------e-d-e------
+# --------e--------
+
+def print_rangoli(n):
+
     import string
-    alphabet = string.ascii_lowercase[:size]
-    width = 4 * size - 3
+    alphabet = string.ascii_lowercase[:n]
+    width = 4 * n - 3
     lines = []
-    for i in range(size):
-        left_part = alphabet[size-1:i:-1]
-        right_part = alphabet[i:size]
+    for i in range(n):
+        left_part = alphabet[n-1:i:-1]
+        right_part = alphabet[i:n]
         line = '-'.join(left_part + right_part)
         lines.append(line.center(width, '-'))
     full_pattern = '\n'.join(lines[::-1] + lines[1:])
-    return full_pattern
+    print(full_pattern)
 
 
 
-print(rangoli(5))
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
