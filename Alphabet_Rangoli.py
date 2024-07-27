aacec358faeaf2e1c181b2d0fbb33720821fa24d
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-07-27 12:47:15 +0530

    add

---
+++ Alphabet_Rangoli.py
@@ -0,0 +1,16 @@
+def rangoli(size):
+    import string
+    alphabet = string.ascii_lowercase[:size]
+    width = 4 * size - 3
+    lines = []
+    for i in range(size):
+        left_part = alphabet[size - 1:i:-1]
+        right_part = alphabet[i:size]
+        line = '-'.join(left_part + right_part)
+        lines.append(line.center(width, '-'))
+    full_pattern = '\n'.join(lines[::-1] + lines[1:])
+    return full_pattern
+
+
+
+print(rangoli(5))
