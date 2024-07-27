6da33a8bb664e3598580fa6f9f3bd94d2c30006e
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-07-27 19:36:32 +0530

    add

---
+++ Type_2/Sort_Factors.py
@@ -0,0 +1,20 @@
+
+# Sort the given elements in descending order based on the number of factors of each element
+def count_factors(num):
+    factors = 0
+    for i in range(1, num //2+1):
+        if num % i == 0:
+            factors += 1
+    return factors
+
+def sort_by_factors(arr):
+    return sorted(arr, key=count_factors, reverse=True)
+
+def main():
+    arr = [10, 57, 60, 89, 36]
+    print("Original array:", arr)
+    sorted_arr = sort_by_factors(arr)
+    print("Sorted array:", sorted_arr)
+
+if __name__ == "__main__":
+    main()
