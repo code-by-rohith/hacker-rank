ada6e2f92a129114172d5f78a66c7418507d7bbe
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-07-31 21:13:42 +0530

    add

--- Type_1/Matrix_Diag_Sum.py
+++ Type_1/Matrix_Diag_Sum.py
@@ -1,13 +1,13 @@
 def diagonalSum(mat):
    res ,n=0,len(mat)
    for i in range(n):
-       res += mat1[i][i]
-       res+= mat1[i][n-1-i]
+       res += mat[i][i]
+       res+= mat[i][n-1-i]
    return  res - (mat[n//2][n//2] if n%2 else 0)
 
-mat1 = [
+mat = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ]
-print(diagonalSum(mat1))  # Output: 25
+print(diagonalSum(mat))  # Output: 25
